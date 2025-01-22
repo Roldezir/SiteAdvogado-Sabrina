4fa452048b62ade7ae1ac59101dda3b151ffdca0
Roldezir <140123210+Roldezir@users.noreply.github.com>
2025-01-22 11:11:50 -0300

    Segundo Commit - Reenviando arquivos
    No primeiro commit eu enviei os arquivos de forma errada, agora estou enviando de forma correta (sem estar dentro de uma pasta).

---
+++ index.js
@@ -0,0 +1,20 @@
+let whatsappLogo=document.querySelector(".whatsappLogo")
+function mudarWppLogo(){whatsappLogo.src='imagens/whatsappLogo1.png'}
+function voltarWppLogo(){whatsappLogo.src='imagens/whatsappLogo.png'}
+
+let redesLogo1=document.querySelector(".imgRedes1")
+function mudarWppLogo1(){redesLogo1.src='imagens/instagramLogo1.png'}
+function voltarWppLogo1(){redesLogo1.src='imagens/instagramLogo.png'}
+
+let redesLogo2=document.querySelector(".imgRedes2")
+function mudarWppLogo2(){redesLogo2.src='imagens/whatsappLogo1.png'}
+function voltarWppLogo2(){redesLogo2.src='imagens/whatsappLogo.png'}
+
+let redesLogo3=document.querySelector(".imgRedes3")
+function mudarWppLogo3(){redesLogo3.src='imagens/facebookLogo1.png'}
+function voltarWppLogo3(){redesLogo3.src='imagens/facebookLogo.png'}
+
+let redesLogo4=document.querySelector(".imgRedes4")
+function mudarWppLogo4(){redesLogo4.src='imagens/linkedinLogo1.png'}
+function voltarWppLogo4(){redesLogo4.src='imagens/linkedinLogo.png'}
+
