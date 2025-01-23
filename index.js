6ae874f8005ebdd8fa72f4effeb664ba8833ab54
Roldezir <140123210+Roldezir@users.noreply.github.com>
2025-01-23 17:45:52 -0300

    Atualizando Links de contato; Padrão de cores; Css
    Fiz os links de contato irem para as redes sociais da sabrina e do thiago, atualizei a paleta de cores do site e mexi um pouco na responsividade do site para algumas resoluções diferentes.

--- index.js
+++ index.js
@@ -11,10 +11,16 @@ function mudarWppLogo2(){redesLogo2.src='imagens/whatsappLogo1.png'}
 function voltarWppLogo2(){redesLogo2.src='imagens/whatsappLogo.png'}
 
 let redesLogo3=document.querySelector(".imgRedes3")
-function mudarWppLogo3(){redesLogo3.src='imagens/facebookLogo1.png'}
-function voltarWppLogo3(){redesLogo3.src='imagens/facebookLogo.png'}
+function mudarWppLogo3(){redesLogo3.src='imagens/instagramLogo1.png'}
+function voltarWppLogo3(){redesLogo3.src='imagens/instagramLogo.png'}
 
 let redesLogo4=document.querySelector(".imgRedes4")
-function mudarWppLogo4(){redesLogo4.src='imagens/linkedinLogo1.png'}
-function voltarWppLogo4(){redesLogo4.src='imagens/linkedinLogo.png'}
+function mudarWppLogo4(){redesLogo4.src='imagens/whatsappLogo1.png'}
+function voltarWppLogo4(){redesLogo4.src='imagens/whatsappLogo.png'}
 
+
+
+function linkWpp(){open('http://wa.me/5564984073790')}
+function linkInsta(){open('https://www.instagram.com/sabrinaz.advogada/?igsh=MXU1dXhjczg4M3Flcg%3D%3D&utm_source=qr#')}
+function linkWpp1(){open('http://wa.me/5564984106144')}
+function linkInsta1(){open('https://www.instagram.com/thiagosiqueira.advogado?igsh=N2lzZmp5ejBzMHVz&utm_source=qr')}
